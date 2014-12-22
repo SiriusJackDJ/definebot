bf6f9bb08092820e5e09a103341bf7ae6b1380d3
SiriusJackDJ <SiriusJackDJ@users.noreply.github.com>
2014-12-22 18:59:58 +1300

    Update extension.js

--- extension.js
+++ extension.js
@@ -105,6 +105,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
+    $.getScript('https://github.com/SiriusJackDJ/definebot/search?utf8=%E2%9C%93&q=basicbot', extend);
 
 }).call(this);
